dbd516519631cfc85349a9fbbe183979248c0e62
DenisProcyon <denis.shadrin@procyon.ee>
2024-12-12 19:10:15 +0100

    asdjusted api input

--- src/api_server.py
+++ src/api_server.py
@@ -2,18 +2,15 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import joblib
 import pandas as pd
-
 from pathlib import Path
 
-from preprocessor import Preprocessor
-
 model_path = Path(__file__).parent / "best_model.pkl"
+pipeline = joblib.load(model_path)
 
-model = joblib.load(model_path)
-maetadata_path = Path(__file__).parent.parent / "data/Safercar_data.csv"
-metadata = pd.read_csv(maetadata_path)
-
-app = FastAPI(title="CDS Final Project: Decision Tree Prediction API", description="🇺🇦🇺🇦🇺🇦")
+app = FastAPI(
+    title="CDS Final Project: Decision Tree Prediction API",
+    description="🇺🇦🇺🇦🇺🇦"
+)
 
 class CarFeatures(BaseModel):
     Make: str
@@ -22,35 +19,12 @@ class CarFeatures(BaseModel):
     Mileage: int
     Condition: str
 
-def preprocess_input(data: pd.DataFrame):
-    data = data.drop(columns="Model", errors="ignore")
-
-    preprocessor = Preprocessor(data=data, metadata=metadata)
-    pipeline = [
-        (preprocessor.assign_metadata_avg, {"columns": ["OVERALL_STARS", "CURB_WEIGHT", "MIN_GROSS_WEIGHT"]}),
-        (preprocessor.assign_countries, {}),
-        (preprocessor.assign_age, {}),
-        (preprocessor.transform_cat_to_num, {"columns": ["Condition", "Country", "Make"]}),
-    ]
-
-    for func, kwargs in pipeline:
-        preprocessor.data = func(**kwargs)
-
-    cleaned_data = preprocessor.data
-    print("Cleaned data:\n", cleaned_data)
-
-    return cleaned_data
-
 @app.post("/predict", summary="Make a car price prediction")
-async def predict(cars: list[CarFeatures]):
+async def predict(car: CarFeatures):
     try:
-        cars_data = pd.DataFrame([car.dict() for car in cars])
-        print("Input data as DataFrame:\n", cars_data)
-
-        features = preprocess_input(cars_data)
-
-        predictions = model.predict(features)
-        return {"predictions": predictions.tolist()}
+        car_data = pd.DataFrame([car.dict()])
+        prediction = pipeline.predict(car_data)
+        return {"prediction": float(prediction[0])}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
